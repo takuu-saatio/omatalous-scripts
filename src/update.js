f9b2a062fd8e063b7f8305ee0e7efc546678a31f
Vladimir Halme <vhalme@gmail.com>
2016-02-22 16:51:02 +0200

    Flexible creation date for copies

--- src/update.js
+++ src/update.js
@@ -71,6 +71,7 @@ Transaction.selectAll({
         delete copy.repeatValue;
         copy.type = "copy";
         copy.month = getCurrentMonth();
+        copy.createdDate = now;
 
         copy = await Transaction.schema.create(copy);
         console.log("Created copy", copy.uuid); 
